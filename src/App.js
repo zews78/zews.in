6ce909e49e2cf748a16606a8534f9e7203ac29ef
zews78 <shariquealam52@gmail.com>
2021-12-23 21:46:45 +0530

    last fucking commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
 import Home from './components/home';
-
+import About from './components/about';
+import Projects, { Projects2, Projects3, Projects4 } from './components/projects';
+import Contact from './components/contact';
 function App() {
+  const a = "dark";
+
   return (
-    <div>
+    <div class={a.mode +" containers"}>
       <Navbar />
-      <Home />
+      <section class="child"><Home /></section>
+      <section class="child"><About /></section>
+
+    <section class="projCont">
+      <section class="child"><Projects /></section>
+      <section class="child"><Projects2 /></section>
+      <section class="child"><Projects3 /></section>
+      <section class="child"><Projects4 /></section>
+    </section>
+    <section class="child"><Contact /></section>
+
+      {/* </div> */}
+      {/* <Learn /> */}
     </div>
 
   );
